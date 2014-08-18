76c70f455a1a652ffb261389c84a9758425113d9
Sarah M Pearson <sarahp@me.com>
2014-08-18 15:10:39 -0500

    a whole lotta stuff added

--- app/controllers/vacations.js
+++ app/controllers/vacations.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var Vacation = require('../models/vacation'),
-    moment   = require('moment');
+    moment   = require('moment'),
+    mp       = require('multiparty');
 
 exports.init = function(req, res){
   res.render('vacations/init');
@@ -16,5 +17,29 @@ exports.create = function(req, res){
 exports.index = function(req, res){
   Vacation.all(function(err, dog){
     res.render('vacations/index', {vacations:dog, moment:moment});
+  });
+};
+
+exports.show = function(req, res){
+  Vacation.findById(req.params.id, function(dog){
+    res.render('vacations/show', {vacation:dog, moment:moment});
+  });
+};
+
+exports.downloadPhoto = function(req, res){
+  Vacation.findById(req.params.id, function(vacation){
+    vacation.downloadPhoto(req.body.url, function(){
+      res.redirect('/vacations/' + req.params.id);
+    });
+  });
+};
+exports.uploadPhoto = function(req, res){
+  Vacation.findById(req.params.id, function(vacation){
+    var form = new mp.Form();
+    form.parse(req, function(err, fields, files){
+      vacation.uploadPhoto(files, function(){
+        res.redirect('/vacations/' + req.params.id);
+      });
+    });
   });
 };
